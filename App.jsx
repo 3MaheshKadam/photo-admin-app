f152354a79038b026438006e3a36815fa354d01e
3MaheshKadam <maheshkadam9298@gmail.com>
2025-09-19 18:42:07 +0530

    nativewind css with jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "./global.css"
+const App = () => {
+  return (
+    <View className='bg-red-400 p-8 mt-8'>
+      <Text>App</Text>
+    </View>
+  )
+}
+
+export default App
